0dccbf0a847e10217c561d8f64091dff8165e269
Agustin Croce <agustinc@exo.com.ar>
2017-01-02 17:05:30 -0300

    Added validations to baucis

--- build-baucis.js
+++ build-baucis.js
@@ -5,6 +5,10 @@ const models     = require('./lib/models');
 const decorators = require('./lib/decorators');
 
 function buildBaucis() {
+    if (decorators.All) {
+        baucis.Controller.decorators(decorators.All);
+    }
+
     Object.keys(models).forEach((modelName) => {
         const controller = baucis.rest(models[modelName]);
         if (decorators.hasOwnProperty(modelName)) {
